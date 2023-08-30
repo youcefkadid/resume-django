65b359f596a0dbc3dc37c7a77f299328f218b722
youcefkadid <ykadid36@gmail.com>
2023-08-30 21:05:36 +0100

    update

--- personal/urls.py
+++ personal/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path 
+from django.views.generic import TemplateView
+
 
 from . import views
 
@@ -6,13 +8,15 @@ from . import views
 urlpatterns=[
 
     
-path('', views.main , name='main'),
-path('about', views.about, name='about'),
+path('', views.about , name='about'),
 path('experience', views.experience, name='experience'),
 path('education', views.education, name='education'),
 path('skills', views.skills, name='skills'),
 path('interests', views.interests, name='interests'),
 path('awards', views.awards, name='awards'),
+path('robots.txt', TemplateView.as_view(
+        template_name="robots.txt", content_type='text/plain')),
+
 
 
 
